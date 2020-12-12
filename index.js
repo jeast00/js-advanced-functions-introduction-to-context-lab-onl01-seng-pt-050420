a8077f26db6f06a9b4c262acfb4bc6cc6569b4a2
Jonathan East <jonathan_east@ymail.com>
2020-12-12 14:39:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const createEmployeeRecord = function(row) {
   return {
     firstName: row[0],
     familyName: row[1],
+    title: row[2],
+    payPerHour: [3],
+    timeInEvents: [],
+    timeOutEvents: []
   }
 }
